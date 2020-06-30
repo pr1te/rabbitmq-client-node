14e863e22a229a01140fa0a8adc2b09e3c009983
Jugkapong Pooban <j.pooban@gmail.com>
2020-06-30 16:01:40 +0700

    feat(index.js): export instance

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 const { Client } = require('./lib/client');
 const ExchangeType = require('./lib/exchange-type');
 
+module.exports = new Client();
 module.exports.Client = Client;
 module.exports.ExchangeType = ExchangeType;
